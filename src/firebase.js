fe7106c784aa2781126bf0bce02b82cc759a63fa
gigialcaraz <115032403+gigialc@users.noreply.github.com>
2024-02-24 01:11:31 -0500

    deployed

--- src/firebase.js
+++ src/firebase.js
@@ -1,26 +1,28 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-
+import "firebase/auth";
+import "firebase/firestore";
+import { getFirestore } from "firebase/firestore"; 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBmN_FaKypjOKQQFrR91ClS76B0YG8bNZ0",
-  authDomain: "stakethestigma-dc208.firebaseapp.com",
-  projectId: "stakethestigma-dc208",
-  storageBucket: "stakethestigma-dc208.appspot.com",
-  messagingSenderId: "748785801153",
-  appId: "1:748785801153:web:b5181a696b692c3f024140",
-  measurementId: "G-H1E8HTJJJ4"
+  apiKey: "AIzaSyBlcx2YFJrJBu85IDpM1UAlO1MN3pf78_c",
+  authDomain: "older-sister-info.firebaseapp.com",
+  projectId: "older-sister-info",
+  storageBucket: "older-sister-info.appspot.com",
+  messagingSenderId: "865074943413",
+  appId: "1:865074943413:web:d59f4cb420cf9d0feeb5fc",
+  measurementId: "G-ZT8F74SWP0"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+
+const db = getFirestore(app);
+
+export { db };
