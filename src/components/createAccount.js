ba178da9641bd315d96a61ad13ae69dbcb5bc989
gigialcaraz <115032403+gigialc@users.noreply.github.com>
2024-06-17 01:02:24 -0400

    hi

--- src/components/createAccount.js
+++ src/components/createAccount.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { getAuth } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import { getFirestore, doc, setDoc } from "firebase/firestore"; // Import Firestore functions
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import styles from "./styles/createAccount.module.css";
@@ -36,32 +37,39 @@ function CreateAccount() {
         setAge(event.target.value);
     };
 
-    const handleCreateAccount = (e) => {
+    const handleCreateAccount = async (e) => {
         e.preventDefault();
         const auth = getAuth();
-        createUserWithEmailAndPassword(auth, email, password)
-        //save other user data
-            .then((authUser) => {
-                if (authUser) {
-                    // Save additional user information (username, city, age) to your database here
-                    navigate("/topics");
-                }
-            })
-            .catch((error) => setError(error.message)); // Set error state
+        const firestore = getFirestore(); // Initialize Firestore// Reference to the user's document in Firestore
+
+        try {
+            const authUser = await createUserWithEmailAndPassword(auth, email, password);
+            if (authUser) {
+                // Save additional user information to Firestore
+                const userRef = doc(firestore, "users", auth.currentUser.uid); 
+                setDoc(userRef, { email: auth.currentUser.email,  
+                    username: username,
+                    city: city,
+                    age: age             
+                }, { merge: true })
+                navigate("/topics");
+            }
+        } catch (error) {
+            setError(error.message); // Set error state
+        }
     };
 
     return (
-
         <div className={styles.container}>
             <br />
             <br />
-        <div className={styles.logo}>
-            <img src={logo} alt="Older Sister Logo" className={styles.logoImage} />
-            <h1>Older Sister</h1>
-        </div>
-        <div className={styles.welcome}>
-            <h2>Create Account!</h2>
-        </div>
+            <div className={styles.logo}>
+                <img src={logo} alt="Older Sister Logo" className={styles.logoImage} />
+                <h1>Older Sister</h1>
+            </div>
+            <div className={styles.welcome}>
+                <h2>Create Account!</h2>
+            </div>
             <br />
             <div className={styles.formContainer}>
                 <form className={styles.form} onSubmit={handleCreateAccount}>
@@ -127,3 +135,4 @@ function CreateAccount() {
 }
 
 export default CreateAccount;
+
